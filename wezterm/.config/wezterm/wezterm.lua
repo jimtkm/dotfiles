d7d4245e1e93253dda43b2e5f1b22835365bc5d2
Jimmy Tan <jimtkm@gmail.com>
2025-05-12 13:10:19 +0800

    typescript for neovim. updated wezterm configuration

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -12,7 +12,7 @@ config.enable_tab_bar = false
 
 config.window_decorations = "RESIZE"
 
-config.window_background_opacity = 0.8
-config.macos_window_background_blur = 10
+-- config.window_background_opacity = 0.8
+-- config.macos_window_background_blur = 10
 
 return config
